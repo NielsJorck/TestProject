c8f614b7eae8723a8bee148ba43062994c9dcd56
Niels Jorck Worm <nw@autoit.dk>
2024-05-20 16:34:32 +0200

    Added favicon

--- pages/index.vue
+++ pages/index.vue
@@ -6,3 +6,28 @@
     <CalculatorLead />
     
 </template>
+
+<script setup>
+
+useHead({
+link: [{
+    rel: 'icon',
+    href: '/img/favicon/FF-Favicon-150x150.jpg',
+    sizes: '32x32'
+},
+{
+    rel: 'icon',
+    href: '/img/favicon/FF-Favicon-300x300.jpg',
+    sizes: '192x192'
+},
+{
+    rel: 'apple-touch-icon',
+    href: '/img/favicon/FF-Favicon-300x300.jpg'
+}],
+meta: [
+{
+    name: 'msapplication-TileImage',
+    content: '/img/favicon/FF-Favicon-300x300.jpg'
+}]
+})
+</script>
