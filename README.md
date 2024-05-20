adee839d5884d7252bbb44305192117efe489440
Niels Jorck Worm <nw@autoit.dk>
2024-05-20 16:21:44 +0200

    TODOs

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
 
+## TODO
+
+* Styling
+* Menu
+* Translations (i18n)
+
 ## Setup
 
 Make sure to install the dependencies:
--- components/CalculatorLead.vue
+++ components/CalculatorLead.vue
@@ -1,6 +1,10 @@
 <template>
-    IKKE SIKKER PÅ, HVOR DU SKAL STARTE?
-    Brug min gratis kalorieberegner nu til at regne ud, hvor meget du skal spise for at nå dine mål.
+    <h2>
+        IKKE SIKKER PÅ, HVOR DU SKAL STARTE?
+    </h2>
+    <p>
+        Brug min gratis kalorieberegner nu til at regne ud, hvor meget du skal spise for at nå dine mål.
+    </p>
     <NuxtLink to="/calorie-calculator">Brug nu</NuxtLink>
     <NuxtImg src="/img/Forsh-Fitness-Calorie-Calculator.png" />
 </template>
